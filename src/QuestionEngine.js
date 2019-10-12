4ec168e2b6141ff8706520b328bbacc760acd74c
Cryptoclysm <cryptoclysm@gmail.com>
2019-10-12 19:14:23 +0100

    Fairly finished. I mean it's not that complicated
    Signed-off-by: Cryptoclysm <cryptoclysm@gmail.com>

--- src/QuestionEngine.js
+++ src/QuestionEngine.js
@@ -1,17 +1,44 @@
 var fs = require("fs");
 
-var jsonFile = fs.readFileSync('./question-bank/data.json');
+var jsonFile = fs.readFileSync('./question-bank/conditionalQuestions.json');
+var jsonFile2 = fs.readFileSync('./question-bank/awaysQuestions.json');
 
-var jsonContent = JSON.parse(jsonFile);
-function getQuestion() {
+var conditionalJSON = JSON.parse(jsonFile);
+var alwaysJSON = JSON.parse(jsonFile);
+
+var exampleVariables = {"Sociable" : 0, "Active" : 0, "Productive" : 0, "Happy" : 0, "Sleep" : 0, "Study": 0};
+
+function getQuestion(questionIndex, variables) { //0 index please
     console.log("Hello World!");
 
-    for(var i = 0; i < jsonContent.length; i++) {
-        console.log("ID: " + jsonContent[i].ID);
-        //newPerson.lastname = jsonContent[i].lastname;
-        //newPerson.age = jsonContent[i].age;
-        //newPerson.save(function (err) {});
+    if(i < alwaysJSON.length){
+        return alwaysJSON[i];
     }
+
+    var tempQNum = alwaysJSON.length; 
+
+    for(var i = 0; i < conditionalJSON.length; i++) {
+        console.log("ID: " + conditionalJSON[i].ID);
+        
+        console.log("Variable Requirement to ask question" + conditionalJSON[i]["Variable Requirement to ask question"]);
+        if(conditionalJSON[i]["Condition"] == "<"){
+            if(variables[conditionalJSON[i]["Variable Requirement to ask question"]] < conditionalJSON[i]["Input Variable Value"]){
+                if(tempQNum == questionIndex){
+                    return conditionalJSON[i];
+                }
+            }
+        }else{
+            if(variables[conditionalJSON[i]["Variable Requirement to ask question"]] > conditionalJSON[i]["Input Variable Value"]){
+                if(tempQNum == questionIndex){
+                    return conditionalJSON[i];
+                }
+            }
+        }
+        
+        tempQNum += 1;
+    }
+
+    return null; //No question to give
 }
 
 module.exports = {
