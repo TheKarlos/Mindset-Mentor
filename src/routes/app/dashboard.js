19ae32f288f8869f3d5d552ac451121ab721e969
Karthikeyan Arumugam <kamtechie@gmail.com>
2019-10-13 09:32:13 +0100

    recommendations updated

--- src/routes/app/dashboard.js
+++ src/routes/app/dashboard.js
@@ -2,13 +2,13 @@
 // routes to render the recommendation page
 
 const dashboardRoute = require('express').Router();
-const params = ["Happiness","Sociability","Activeness","Productivity","StressAnxiety","Sleep"]
+const params = ["Happiness","Sociability","Activeness","Productivity","Loneliness","Sleep"]
 const traitSuggestions = {
     "Happiness": ["Open up to a friend about how you're feeling", "Try calling Nightline to talk about any problems", "Go to your local support group at Leeds Church"],
     "Sociability": ["Try to join a new club this week", "Text a friend you've lost touch with", "Go to the local football team"],
     "Activeness": ["Go for a jog today", "Try visiting the gym", "Play some football later"],
     "Productivity": ["Find a personal subject mentor", "Talk to your teacher about any problems", "Create a daily schedule"],
-    "StressAnxiety": ["Test", "Test", "Test"],
+    "Loneliness": ["Join a class where everyone is focused on an activity (so there is no need to interact)", "Join the Mind Matters society at Leeds University (luumindmatterssociety.wordpress.com)", "Go to the local health and healing festival (healthhealingfestivals.co.uk)"],
     "Sleep": ["Test", "Test", "Test"]
 }
 
